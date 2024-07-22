ec8314cabc39a0bc9b734a1942828d78f055890d
Tadiwa Lesley Musekiwa <lesliemusekiwa@gmail.com>
2024-07-22 23:48:04 +0200

    Frontend Fix: Navbar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,10 @@
-import { Box, Flex, Text, Button, Link, Heading } from "@chakra-ui/react"
+import { Box, Flex, Text, Button, Link, Heading } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../Authentication/AuthProvider";
 import { useLogout } from "../../hooks/useLogin";
 
 const MenuItem = (props: any) => {
-  const { children, isLast, ...rest } = props
+  const { children, isLast, ...rest } = props;
   return (
     <Text
       mb={{ base: isLast ? 0 : 8, sm: 0 }}
@@ -12,17 +12,15 @@ const MenuItem = (props: any) => {
       display="block"
       {...rest}
     >
-      <Link >{children}</Link>
+      <Link>{children}</Link>
     </Text>
-  )
-}
+  );
+};
 
 const Header = (props: any) => {
   const navigate = useNavigate();
-  const {logout} = useLogout()
-  const {user} = useAuth();
-  const logStatus = user ? "LogOut" : "LogIn" 
-  console.log(user)
+  const { logout } = useLogout();
+  const { user } = useAuth();
   return (
     <Flex
       as="nav"
@@ -35,43 +33,64 @@ const Header = (props: any) => {
       bg={["primary.500", "primary.500", "transparent", "transparent"]}
       {...props}
     >
-      <Flex align="center">
-       { <Heading>LesleyOnline!!!!</Heading>}
-      </Flex>
+      <Flex align="center">{<Heading>LesleyOnline!!!!</Heading>}</Flex>
 
-      <Box
-        flexBasis={{ base: "100%", md: "auto" }}
-      >
+      <Box flexBasis={{ base: "100%", md: "auto" }}>
         <Flex
           align={["center", "center", "center", "center"]}
           justify={["center", "space-between", "flex-end", "flex-end"]}
           direction={["column", "row", "row", "row"]}
           pt={[4, 4, 0, 0]}
         >
-          <MenuItem onClick={() => navigate("/")} fontSize='16px' fontWeight="bold">Home</MenuItem>
-          <MenuItem onClick={() => navigate("/profile")} fontSize='16px' fontWeight='bold'>Profile</MenuItem>
-          <MenuItem onClick={() => navigate("/checkout")} fontSize='16px' fontWeight="bold">Cart</MenuItem>
-          <MenuItem >
-            <Button
-              size="sm"
-              rounded="md"
-              _hover={{
-                bg: [
-                  "primary.100",
-                  "primary.100",
-                  "primary.600",
-                  "primary.600",
-                ],
-              }}
-              onClick={() => logout()}
-            >
-             { logStatus }
-            </Button>
-          </MenuItem>
+          {user ? (
+            <>
+              <MenuItem
+                onClick={() => navigate("/")}
+                fontSize="16px"
+                fontWeight="bold"
+              >
+                Home
+              </MenuItem>
+              <MenuItem
+                onClick={() => navigate("/profile")}
+                fontSize="16px"
+                fontWeight="bold"
+              >
+                Profile
+              </MenuItem>
+              <MenuItem
+                onClick={() => navigate("/checkout")}
+                fontSize="16px"
+                fontWeight="bold"
+              >
+                Cart
+              </MenuItem>
+
+              <MenuItem>
+                <Button
+                  size="sm"
+                  rounded="md"
+                  _hover={{
+                    bg: [
+                      "primary.100",
+                      "primary.100",
+                      "primary.600",
+                      "primary.600",
+                    ],
+                  }}
+                  onClick={() => logout()}
+                >
+                  {"LogOut"}
+                </Button>
+              </MenuItem>
+            </>
+          ) : (
+            <></>
+          )}
         </Flex>
       </Box>
     </Flex>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
